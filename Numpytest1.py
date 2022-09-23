50c3bd4267b6ec0adb1d0dbf71e019c893f9a56d
Montissol Stevens <95543127+Cyberboyht@users.noreply.github.com>
2022-09-23 13:28:49 -0400

    Firts Commit

---
+++ Numpytest1.py
@@ -0,0 +1,52 @@
+import numpy as np
+def fonction_test(x,y,z):
+    resultat= x+y*z
+    fonction_test (2,3,4)
+    return resultat
+
+#A=np.array([[1,2], [3,4], [5,6]])
+#print (A)
+#print (A.sum)
+#print (A.shape)
+#B=A.T
+#print (B)
+#print (B.shape)
+#C=np.ones((3,2))
+#print (C)
+#C.T
+#print(A.dot(C.T))
+#x=4
+#y=6
+#z=(x**2)
+#print(z)
+#signal=0
+#if signal==1:
+   # print("Alerte")
+    #Pour arreter quelques choses 
+#elif signal>=0:
+ #   print("Attention")
+#else:
+ #   print("No alerte")
+    #Pour avancer
+
+#for i in range(0,30):
+    #print ("Machine Learning")
+    
+#for i in range(0,50):
+ #   print (i)
+
+#i=0
+#while i<10:
+ #   print(i)
+  #  i=i+1
+
+
+    
+
+
+
+
+
+
+
+
